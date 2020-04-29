57419829914c5659f7b4381bb32244cb069389fa
Y. Violet Guo <yueguo598@gmail.com>
2020-04-29 16:02:47 -0300

    solve temp conv

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,14 @@
-const ftoc = function() {
-
+const ftoc = function(tempF) {
+  let temp = (tempF - 32) * 5/9;
+  temp = Math.round(temp * 10) / 10;
+  return temp;
 }
 
-const ctof = function() {
+const ctof = function(tempC) {
+  let temp = tempC *9/5 + 32;
+  temp = Math.round(temp * 10) / 10;
 
+  return temp;
 }
 
 module.exports = {
