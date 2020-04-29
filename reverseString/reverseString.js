6c1bc616a7f4fde7c62ceb7255ea0ed40380369b
Y. Violet Guo <yueguo598@gmail.com>
2020-04-29 14:35:47 -0300

    finish exe 1-3

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,16 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let new_str = '';
 
+    // wrong but same result
+    // for(let i = str.length; i--; i>=0){
+    //     new_str = new_str.concat(str[i]);
+    // }
+
+    // the correct for loop
+    for(let i = str.length -1 ; i>=0; i--){
+        new_str = new_str.concat(str[i]);
+    }
+    return new_str;
 }
 
 module.exports = reverseString
