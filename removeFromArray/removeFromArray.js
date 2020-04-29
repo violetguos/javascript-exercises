e808ba71fb473d6ebf6eb08db583bdb7f0468d62
Y. Violet Guo <yueguo598@gmail.com>
2020-04-29 14:52:45 -0300

    finsh remove

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
 const removeFromArray = function() {
-
+    let arr=arguments[0];
+    for(let i = 1; i<arguments.length; i++){
+        if (arr.includes(arguments[i])){
+            arr.splice(arr.indexOf(arguments[i]), 1 );
+        }
+    }
+    return arr;
 }
 
 module.exports = removeFromArray
--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(str, n) {
+    if (n==0)
+        return('');
+    if(n<0)
+        return 'ERROR';
 
+    let str_res = str;
+    // start from 1 on purpose
+    for (let i =1; i<n; i++){
+        str_res = str_res.concat(str);
+    }
+    console.log(str_res);
+    return str_res;
 }
 
 module.exports = repeatString
