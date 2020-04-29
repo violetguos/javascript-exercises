82992c64c84b730061dda7be6e1b62e4dd1c209c
Y. Violet Guo <yueguo598@gmail.com>
2020-04-29 15:53:47 -0300

    solve sum all

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+function isNumber(a){
+    return typeof a == 'number'?true:false;
+}
 
+const sumAll = function(lowerBound, upperBound) {
+    if(!(isNumber(lowerBound) && isNumber(upperBound)))
+        return 'ERROR';
+    if (lowerBound<0 | upperBound <0)
+        return 'ERROR';
+    else{
+        if (lowerBound >upperBound){
+            let temp = lowerBound;
+            lowerBound = upperBound;
+            upperBound = temp;
+        }
+
+        let sum = 0;
+        for(let i=lowerBound; i<=upperBound; i++)
+            sum += i;
+        return sum;
+    }
+    
 }
 
 module.exports = sumAll
