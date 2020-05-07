c9d25507743108db59b311c6a4b899a1894b9cad
Y. Violet Guo <yueguo598@gmail.com>
2020-05-07 11:15:08 -0300

    Finish palindrome

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,31 @@
-const palindromes = function() {
+function reverseString(str) {
+    // Step 1. Use the split() method to return a new array
+    var splitString = str.split(""); // var splitString = "hello".split("");
+    // ["h", "e", "l", "l", "o"]
+ 
+    // Step 2. Use the reverse() method to reverse the new created array
+    var reverseArray = splitString.reverse(); // var reverseArray = ["h", "e", "l", "l", "o"].reverse();
+    // ["o", "l", "l", "e", "h"]
+ 
+    // Step 3. Use the join() method to join all elements of the array into a string
+    var joinArray = reverseArray.join(""); // var joinArray = ["o", "l", "l", "e", "h"].join("");
+    // "olleh"
+    
+    //Step 4. Return the reversed string
+    return joinArray; // "olleh"
+}
 
+function deletePunctuation(str){
+    return str.replace(/(~|`|!|@|#|$|%|^|&|\*|\(|\)|{|}|\[|\]|;|:|\"|'|<|,|\.|>|\?|\/|\\|\||-|_|\s+|\+|=)/g,"");
+    
+}
+
+const palindromes = function(str) {
+    let originalStr = deletePunctuation(str);
+    originalStr = originalStr.toLowerCase();
+    let reversedStr = reverseString(originalStr);
+    let compare = originalStr.localeCompare(reversedStr);
+    return (compare == 0)?true:false;
 }
 
 module.exports = palindromes
